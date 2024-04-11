102ec7296b53a5d4663409fa7c817804568af421
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-04-11 12:16:50 -0300

    betterBiruleibes

--- .venv/Scripts/index.py
+++ .venv/Scripts/index.py
@@ -1,29 +1,35 @@
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 import flask
 from db import *
 
 app = Flask(__name__)
 
+app.secret_key = 'thatsSecret';
+
 @app.route("/", methods = ['POST','GET'])
 def index():
-   if(flask.request.method == "POST"):
-    show = 0;
-    showError = 0
+ if(flask.request.method == "POST"):
+    
     userEmail = request.form['email'];
-    userPassword = request.form['password'];
+    userPassword = request.form['password']; 
+    loginError = 0
         
     if(userEmail == row[4] and userPassword == row[5]):
-      show = 1;
+        session["email"] = userEmail;
+        
+      
+        return render_template('index.htm',login = session["email"]); 
 
+    else:
+
+       loginError = 1
     
-    return render_template('index.htm',show = show); 
+       return render_template('index.htm',loginError = loginError);
 
-   else:
-    showError = 1
-    show = 0
-    return render_template('index.htm',showError = showError)
- 
+ else:
+  return render_template('index.htm');
     
      
       
